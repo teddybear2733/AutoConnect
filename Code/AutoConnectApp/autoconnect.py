2ec24cbeac857634ec37f9273a04b3286b5c8549
Armen K <armenkrikorian2722@gmail.com>
2023-03-15 21:18:55 -0400

    ADded animations

--- Code/AutoConnectApp/autoconnect.py
+++ Code/AutoConnectApp/autoconnect.py
@@ -1,71 +1,76 @@
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.config import Config
+from kivy.lang import Builder
 from kivy.core.window import Window
-from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
-
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ListProperty, NumericProperty
+from kivy.graphics import *
 import os
-import subprocess
+import time
+from threading import Thread
+# Create both screens. Please note the root.manager.current: this is how
+# you can control the ScreenManager from kv. Each screen has by default a
+# property manager that gives you the instance of the ScreenManager used.
+Builder.load_file('screens.kv')
 
-class MainMenu(Screen):
+            
+
+# Declare both screens
+class MenuScreen(Screen):
+
+    wPadding = (Window.size[0] / 16)
+    wSpacingx = (Window.size[1] / 10.66) + (wPadding / 1.5)
+    wSpacingy = (Window.size[0] / 16)
+    widget_size_x = (Window.size[1] - (2 * wPadding) - wSpacingx) / 2
+    widget_size_y = (Window.size[1] - (2 * wPadding) - wSpacingx) / 2
+
+    def open_app(self, app_path):
+        thread = Thread(target=self.task,  args=(app_path,))
+        thread.start()
+
+    def task(self, app_path):
+        applicationsFolder = os.path.join(os.path.dirname(__file__), "Applications")
+        appLaunchScript = os.path.join(applicationsFolder, app_path)
+        os.system(appLaunchScript)
+        #self.manager.current = "MainMenu"
+
+class SettingsScreen(Screen):
+    #bckColor = (30/255, 30/255, 30/255, 255/255)
+
+    def change_color(self):
+        pass
+        #print(self.bckColor)
+        #self.bckColor = (1,1,1,1)
+        #print(self.bckColor)
+
+class LoadingScreen(Screen):
+    
     pass
+        
+    
 
-class Settings(Screen):
-    pass
+class TestApp(App):
 
-class AutoConnect(App):
+    def switch_screens(self):
+        pass
+
     def build(self):
-
+        # Create the screen manager
         self.sm = ScreenManager()
 
-        wPadding = (Window.size[0] / 16)
-        wSpacingx = (Window.size[1] / 10.66) + (wPadding / 1.5)
-        wSpacingy = (Window.size[0] / 16)
-        
-        widget_size_x = (Window.size[1] - (2 * wPadding) - wSpacingx) / 2
-        widget_size_y = (Window.size[1] - (2 * wPadding) - wSpacingx) / 2
+        # Dark Mode
+        self.sm.add_widget(MenuScreen(name='MainMenu'))
+        self.sm.add_widget(LoadingScreen(name='Loading'))
+        self.sm.add_widget(SettingsScreen(name='Settings'))
 
-        root_widget = BoxLayout(orientation='vertical', padding=wPadding)
-        
-        button_grid = GridLayout(cols=3, spacing=[(Window.size[1] / 10.66) + (wPadding / 1.5), wSpacing])
-        
-        efi = Button(background_normal = 'EFI2.png', height=widget_size_y, width=widget_size_x, size_hint_x=None, size_hint_y=None)
-        pe = Button(background_normal = 'PE.png', height=widget_size_y, width=widget_size_x, size_hint_x=None, size_hint_y=None)
-        MegaLogViewer = Button(background_normal = 'MGLV.png', height=widget_size_y, width=widget_size_x, size_hint_x=None, size_hint_y=None)
-        AndroidAuto = Button(background_normal = 'AndroidAuto.png', height=widget_size_y, width=widget_size_x, size_hint_x=None, size_hint_y=None)
-        Settings = Button(background_normal = 'Settings.png', height=widget_size_y, width=widget_size_x, size_hint_x=None, size_hint_y=None)
-        
-        button_grid.add_widget(efi)
-        button_grid.add_widget(pe)
-        button_grid.add_widget(AndroidAuto)
-        button_grid.add_widget(MegaLogViewer)
-        button_grid.add_widget(Button(text="TBD", height=widget_size_y, width=widget_size_x, size_hint_x=None, size_hint_y=None))
-        button_grid.add_widget(Settings)
+        # Light Mode
+        # self.sm.add_widget(MenuScreenLight(name='MainMenuLight'))
+        # self.sm.add_widget(LoadingScreenLight(name='LoadingLight'))
+        # self.sm.add_widget(SettingsScreenLight(name='SettingsLight'))
 
-        efi.bind(on_press = self.open_tuner_studio)
-        MegaLogViewer.bind(on_press = self.open_mglv)
-
-        root_widget.add_widget(button_grid)
-
-        self.sm.add_widget(root_widget)
         return self.sm
-    
-    def change_screen():
-        pass
-    
-    def open_tuner_studio(self, event):
-        self.sm.switch_to()
-        #os.system("open /Applications/TunerStudio_MS.app/")
-         
 
-    def open_mglv(self, event):
-        #print("hi")
-        subprocess.run(["open", "/Applications/MegaLogViewerHD.app/"])
-        #os.system("open /Applications/MegaLogViewerHD.app/")
+if __name__ == '__main__':
+    #Window.fullscreen = True
 
-Window.fullscreen = True
-#Window.maximize()
-AutoConnect().run()
+    TestApp().run()
+    
